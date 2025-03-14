e7a79a811b820520e9c24576d58a4439cfe23483
Thiago Villani <villani.thiago01@gmail.com>
2025-03-14 22:05:11 +0100

    add contact form script, fix css

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,3 +1,12 @@
+// adicionar active nos links
+$(document).ready(function () {
+  $(".navbar .nav-link").on("click", function () {
+    $(".navbar").find(".active").removeClass("active");
+    $(this).addClass("active");
+  });
+});
+
+//carroussel
 $(document).ready(function () {
   var multipleCardCarousel = document.querySelector("#carouselExampleControls");
   if (window.matchMedia("(min-width: 576px)").matches) {
@@ -36,3 +45,38 @@ $(document).ready(function () {
     $(multipleCardCarousel).addClass("slide");
   }
 });
+
+//envio email
+/*$(document).ready(function () {
+  $("#contact-form").on("submit", function (event) {
+    event.preventDefault();
+
+    var data = {
+      name: $("#nomeInput").val(),
+      email: $("#emailInput").val(),
+      message: $("#areaMensagemInput").val(),
+    };
+
+    $("#submitBtn").prop("disabled", true);
+
+    $.ajax("http://localhost:8080/api/v1/send-contact-email", {
+      type: "POST",
+      data: JSON.stringify(data),
+      contentType: "application/json",
+      crossDomain: true,
+      dataType: "text", //representa a responsa do servidor que é void ""
+      timeout: 2000, // timeout para 2 segundos
+    })
+      .done(function () {
+        alert("Email enviado!");
+        document.getElementById("contact-form").reset();
+        $("#submitBtn").prop("disabled", false);
+      })
+      .fail(function (error) {
+        alert("Oops...Ocorreu um erro, tente mais tarde");
+        console.log(JSON.stringify(error));
+        document.getElementById("contact-form").reset();
+        $("#submitBtn").prop("disabled", false);
+      });
+  });
+});*/
